d18e7d96a9137757c1ae334ea6fd162ca5d6db7f
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-05-26 15:24:10 -0400

    Add files via upload

---
+++ testandodefinicoes.py
@@ -0,0 +1,41 @@
+# compras = ["Pão", "Café", "Leite"]
+# # print(compras[3])
+
+# # Pode ser removida pelonome do produto ou pelo índice 
+# #compras.remove("Café")
+# compras.remove(compras[0])
+# print(compras)
+
+
+# # append acrescenta um item no final da lista, só pode adicionar um item por vez
+# compras.append("Amendoa")
+# print(compras)
+
+
+# #s orted faz uma outra lista receber a lista ordenada (crescente) ou ordem alfabética
+# compras_ordenadas = sorted(compras)
+# print(compras_ordenadas)
+
+# # for percorre a lista, cada vez o "item" receberá um valor da lista ordenadamente
+# for item in compras:
+#     print(f"-{item}")
+
+# Listas = []
+# Dicionários = {chave : valor}
+# Tuplas = ()
+
+# teste = [1, 2, 3, 5]
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+# teste = (1, 2, 3, 5)
+# print(teste)
+# teste.remove(1)
+# print(teste)
+
+# teste = {1, 2, 3, 5, 5}
+# print(teste)
+# teste.remove(1)
+# print(teste)
+#teste.add(3)
